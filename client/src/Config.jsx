1dd4c408fbd8daab5bf03cb5b2b41f694ecaae94
CodeWithAshafak <ashafak04@gmail.com>
2025-10-16 12:13:50 +0530

    render backend added

--- client/src/Config.jsx
+++ client/src/Config.jsx
@@ -1,3 +1,4 @@
-const Base_url = "http://localhost:5000/api"
+// const Base_url = "http://localhost:5000/api"
+const Base_url = "https://load-request-approval.onrender.com"
 
 export default Base_url
